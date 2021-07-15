dc4a7fa7c4c7b194369c1645a38ea6f35a4c12a3
Kirik1105 <kirik1103@gmail.com>
2021-07-15 19:52:36 +0300

    Теперь калькулятор считает

--- app/src/main/java/com/example/gbandroid1lesson2/MainActivity.java
+++ app/src/main/java/com/example/gbandroid1lesson2/MainActivity.java
@@ -11,7 +11,9 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private double calc=0.0;
-    private String calcMemo="";
+    private String calcMemo1="";
+    private String calcMemo2="";
+    private int Casess;
     private TextView textV;
 
     @Override
@@ -24,85 +26,105 @@ public class MainActivity extends AppCompatActivity {
 
     public void button1_onClick(View view){
 
-        calcMemo=calcMemo+"1";
+        calcMemo1=calcMemo1+"1";
         UndateTextView();
     }
     public void button2_onClick(View view){
 
-        calcMemo=calcMemo+"2";
+        calcMemo1=calcMemo1+"2";
         UndateTextView();
     }
     public void button3_onClick(View view){
 
-        calcMemo=calcMemo+"3";
+        calcMemo1=calcMemo1+"3";
         UndateTextView();
     }
     public void button4_onClick(View view){
 
-        calcMemo=calcMemo+"4";
+        calcMemo1=calcMemo1+"4";
         UndateTextView();
     }
     public void button5_onClick(View view){
 
-        calcMemo=calcMemo+"5";
+        calcMemo1=calcMemo1+"5";
         UndateTextView();
     }
     public void button6_onClick(View view){
 
-        calcMemo=calcMemo+"6";
+        calcMemo1=calcMemo1+"6";
         UndateTextView();
     }
     public void button7_onClick(View view){
 
-        calcMemo=calcMemo+"7";
+        calcMemo1=calcMemo1+"7";
         UndateTextView();
     }
     public void button8_onClick(View view){
 
-        calcMemo=calcMemo+"8";
+        calcMemo1=calcMemo1+"8";
         UndateTextView();
     }public void button9_onClick(View view){
 
-        calcMemo=calcMemo+"9";
+        calcMemo1=calcMemo1+"9";
         UndateTextView();
     }
     public void button0_onClick(View view){
 
-        calcMemo=calcMemo+"0";
+        calcMemo1=calcMemo1+"0";
         UndateTextView();
     }
     public void button_point(View view){
 
-        calcMemo=calcMemo+".";
+        calcMemo1=calcMemo1+".";
         UndateTextView();
     }
     public void button_plus(View view){
-
-        calcMemo=calcMemo+"+";
+        Casess=1;
+        calcMemo2 = calcMemo1;
+        calcMemo1="";
         UndateTextView();
     }
     public void button_minus(View view){
-
-        calcMemo=calcMemo+"-";
+        Casess=2;
+        calcMemo2 = calcMemo1;
+        calcMemo1="";
         UndateTextView();
     }
     public void button_add(View view){
-
-        calcMemo=calcMemo+"*";
+        Casess=3;
+        calcMemo2 = calcMemo1;
+        calcMemo1="";
         UndateTextView();
     }
 
     public void button_button_divide(View view){
-
-        calcMemo=calcMemo+"/";
+        Casess=4;
+        calcMemo2 = calcMemo1;
+        calcMemo1="";
         UndateTextView();
     }
 
-    private void UndateTextView(){
-        textV.setText(String.valueOf(calcMemo));
-
+    public void button_equal(View view){
+        switch (Casess){
+            case 1:calc=Double.parseDouble(calcMemo1)+Double.parseDouble(calcMemo2);
+                break;
+            case 2:calc=Double.parseDouble(calcMemo1)-Double.parseDouble(calcMemo2);
+                break;
+            case 3:calc=Double.parseDouble(calcMemo1)*Double.parseDouble(calcMemo2);
+                break;
+            case 4:calc=Double.parseDouble(calcMemo1)/Double.parseDouble(calcMemo2);
+                break;
+        }
+        calcMemo1="";
+        ResultTextView();
     }
 
+    private void UndateTextView(){
+        textV.setText(String.valueOf(calcMemo1));
+    }
+    private void ResultTextView(){
+        textV.setText(String.valueOf(calc));
+    }
 
     @Override
     protected void onStart() {
