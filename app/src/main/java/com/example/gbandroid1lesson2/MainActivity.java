74b1708b5656f3c9a6250d32f0150319efefa37f
Kirik1105 <kirik1103@gmail.com>
2021-07-15 18:58:01 +0300

    Добавил дал кнопкам методы и вывожу данные ввода в окошке

--- app/src/main/java/com/example/gbandroid1lesson2/MainActivity.java
+++ app/src/main/java/com/example/gbandroid1lesson2/MainActivity.java
@@ -3,15 +3,107 @@ package com.example.gbandroid1lesson2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+    private double calc=0.0;
+    private String calcMemo="";
+    private TextView textV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calc_layout);
+        textV=findViewById(R.id.textView);
+
     }
+
+    public void button1_onClick(View view){
+
+        calcMemo=calcMemo+"1";
+        UndateTextView();
+    }
+    public void button2_onClick(View view){
+
+        calcMemo=calcMemo+"2";
+        UndateTextView();
+    }
+    public void button3_onClick(View view){
+
+        calcMemo=calcMemo+"3";
+        UndateTextView();
+    }
+    public void button4_onClick(View view){
+
+        calcMemo=calcMemo+"4";
+        UndateTextView();
+    }
+    public void button5_onClick(View view){
+
+        calcMemo=calcMemo+"5";
+        UndateTextView();
+    }
+    public void button6_onClick(View view){
+
+        calcMemo=calcMemo+"6";
+        UndateTextView();
+    }
+    public void button7_onClick(View view){
+
+        calcMemo=calcMemo+"7";
+        UndateTextView();
+    }
+    public void button8_onClick(View view){
+
+        calcMemo=calcMemo+"8";
+        UndateTextView();
+    }public void button9_onClick(View view){
+
+        calcMemo=calcMemo+"9";
+        UndateTextView();
+    }
+    public void button0_onClick(View view){
+
+        calcMemo=calcMemo+"0";
+        UndateTextView();
+    }
+    public void button_point(View view){
+
+        calcMemo=calcMemo+".";
+        UndateTextView();
+    }
+    public void button_plus(View view){
+
+        calcMemo=calcMemo+"+";
+        UndateTextView();
+    }
+    public void button_minus(View view){
+
+        calcMemo=calcMemo+"-";
+        UndateTextView();
+    }
+    public void button_add(View view){
+
+        calcMemo=calcMemo+"*";
+        UndateTextView();
+    }
+
+    public void button_button_divide(View view){
+
+        calcMemo=calcMemo+"/";
+        UndateTextView();
+    }
+
+    private void UndateTextView(){
+        textV.setText(String.valueOf(calcMemo));
+
+    }
+
+
     @Override
     protected void onStart() {
         super.onStart();
