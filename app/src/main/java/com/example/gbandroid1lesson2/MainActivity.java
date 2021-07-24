abf0c73a8ab4b88c51717d232b0b90d784501a18
Kirik1105 <kirik1103@gmail.com>
2021-07-24 21:12:50 +0300

    Добавил стиль и изменил кнопки, и сдела код читабельней

--- app/src/main/java/com/example/gbandroid1lesson2/MainActivity.java
+++ app/src/main/java/com/example/gbandroid1lesson2/MainActivity.java
@@ -10,9 +10,9 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    private double calc=0.0;
-    private String calcMemo1="";
-    private String calcMemo2="";
+    private double calc = 0.0;
+    private String calcMemo1 = "";
+    private String calcMemo2 = "";
     private int Casess;
     private TextView textV;
 
@@ -20,109 +20,128 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calc_layout);
-        textV=findViewById(R.id.textView);
+        textV = findViewById(R.id.textView);
 
     }
 
-    public void button1_onClick(View view){
+    public void button1_onClick(View view) {
 
-        calcMemo1=calcMemo1+"1";
+        calcMemo1 = calcMemo1 + "1";
         UndateTextView();
     }
-    public void button2_onClick(View view){
 
-        calcMemo1=calcMemo1+"2";
+    public void button2_onClick(View view) {
+
+        calcMemo1 = calcMemo1 + "2";
         UndateTextView();
     }
-    public void button3_onClick(View view){
 
-        calcMemo1=calcMemo1+"3";
+    public void button3_onClick(View view) {
+
+        calcMemo1 = calcMemo1 + "3";
         UndateTextView();
     }
-    public void button4_onClick(View view){
 
-        calcMemo1=calcMemo1+"4";
+    public void button4_onClick(View view) {
+
+        calcMemo1 = calcMemo1 + "4";
         UndateTextView();
     }
-    public void button5_onClick(View view){
 
-        calcMemo1=calcMemo1+"5";
+    public void button5_onClick(View view) {
+
+        calcMemo1 = calcMemo1 + "5";
         UndateTextView();
     }
-    public void button6_onClick(View view){
 
-        calcMemo1=calcMemo1+"6";
+    public void button6_onClick(View view) {
+
+        calcMemo1 = calcMemo1 + "6";
         UndateTextView();
     }
-    public void button7_onClick(View view){
 
-        calcMemo1=calcMemo1+"7";
+    public void button7_onClick(View view) {
+
+        calcMemo1 = calcMemo1 + "7";
         UndateTextView();
     }
-    public void button8_onClick(View view){
 
-        calcMemo1=calcMemo1+"8";
-        UndateTextView();
-    }public void button9_onClick(View view){
+    public void button8_onClick(View view) {
 
-        calcMemo1=calcMemo1+"9";
+        calcMemo1 = calcMemo1 + "8";
         UndateTextView();
     }
-    public void button0_onClick(View view){
 
-        calcMemo1=calcMemo1+"0";
+    public void button9_onClick(View view) {
+
+        calcMemo1 = calcMemo1 + "9";
         UndateTextView();
     }
-    public void button_point(View view){
 
-        calcMemo1=calcMemo1+".";
+    public void button0_onClick(View view) {
+
+        calcMemo1 = calcMemo1 + "0";
         UndateTextView();
     }
-    public void button_plus(View view){
-        Casess=1;
+
+    public void button_point(View view) {
+
+        calcMemo1 = calcMemo1 + ".";
+        UndateTextView();
+    }
+
+    public void button_plus(View view) {
+        Casess = 1;
         calcMemo2 = calcMemo1;
-        calcMemo1="";
-        UndateTextView();
-    }
-    public void button_minus(View view){
-        Casess=2;
-        calcMemo2 = calcMemo1;
-        calcMemo1="";
-        UndateTextView();
-    }
-    public void button_add(View view){
-        Casess=3;
-        calcMemo2 = calcMemo1;
-        calcMemo1="";
+        calcMemo1 = "";
         UndateTextView();
     }
 
-    public void button_button_divide(View view){
-        Casess=4;
+    public void button_minus(View view) {
+        Casess = 2;
         calcMemo2 = calcMemo1;
-        calcMemo1="";
+        calcMemo1 = "";
         UndateTextView();
     }
 
-    public void button_equal(View view){
-        switch (Casess){
-            case 1:calc=Double.parseDouble(calcMemo1)+Double.parseDouble(calcMemo2);
+    public void button_add(View view) {
+        Casess = 3;
+        calcMemo2 = calcMemo1;
+        calcMemo1 = "";
+        UndateTextView();
+    }
+
+    public void button_button_divide(View view) {
+        Casess = 4;
+        calcMemo2 = calcMemo1;
+        calcMemo1 = "";
+        UndateTextView();
+    }
+
+    public void button_equal(View view) {
+        switch (Casess) {
+            case 1:
+                calc = Double.parseDouble(calcMemo1) + Double.parseDouble(calcMemo2);
                 break;
-            case 2:calc=Double.parseDouble(calcMemo1)-Double.parseDouble(calcMemo2);
+            case 2:
+                calc = Double.parseDouble(calcMemo1) - Double.parseDouble(calcMemo2);
                 break;
-            case 3:calc=Double.parseDouble(calcMemo1)*Double.parseDouble(calcMemo2);
+            case 3:
+                calc = Double.parseDouble(calcMemo1) * Double.parseDouble(calcMemo2);
                 break;
-            case 4:calc=Double.parseDouble(calcMemo1)/Double.parseDouble(calcMemo2);
+            case 4:
+                calc = Double.parseDouble(calcMemo1) / Double.parseDouble(calcMemo2);
                 break;
         }
-        calcMemo1="";
+        calcMemo1 = "";
         ResultTextView();
     }
 
-    private void UndateTextView(){
+    private void UndateTextView() {
         textV.setText(String.valueOf(calcMemo1));
     }
-    private void ResultTextView(){
+
+    private void ResultTextView() {
         textV.setText(String.valueOf(calc));
     }
 
